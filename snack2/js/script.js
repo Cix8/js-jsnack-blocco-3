56aefd347b693ef6c8d27941c6e9a86666a3f49b
Cix8 <francesco.natal_fn@libero.it>
2022-05-04 14:23:17 +0200

    add snack2

---
+++ snack2/js/script.js
@@ -0,0 +1,37 @@
+const numbersArray = [];
+
+const limit = 50;
+
+let sum = numbersArray.length;
+
+let userNumber = 0;
+
+let myCondition = true;
+
+while(sum < limit && myCondition) {
+    userNumber = parseInt(prompt('Inserisci un numero'));
+    if (sum + userNumber < limit) {
+        numbersArray.push(userNumber);
+        sum = sumInArray(numbersArray);
+    } else {
+        myCondition = false;
+    }
+    console.log(numbersArray);
+}
+
+alert('Hai superato il limite di 50! La somma finale è pari a:'+sum);
+
+console.log(sum);
+
+//                          FUNCTIONS
+
+function sumInArray(array) {
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        const thisItem = array[i];
+        sum += thisItem;
+    }
+    
+    return sum
+}
